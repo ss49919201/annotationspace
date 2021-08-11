845c2b462ca0329437acb33de082ac42a94a3913
s-beats <sakashin10291029@gmail.com>
2021-08-11 22:32:37 +0900

    feat: regex patern

--- annotationspace.go
+++ annotationspace.go
@@ -2,41 +2,14 @@ package annotationspace
 
 import (
 	"go/ast"
-	"strings"
+	"regexp"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
 )
 
-type annotation int
-
-const (
-	colon      = ":"
-	whitespace = " "
-)
-
-const (
-	TODO annotation = iota
-	FIXME
-	NOTE
-	REFUCTOR
-)
-
-func (a annotation) String() string {
-	switch a {
-	case TODO:
-		return "TODO"
-	case FIXME:
-		return "FIXME"
-	case NOTE:
-		return "NOTE"
-	case REFUCTOR:
-		return "REFUCTOR"
-	default:
-		panic("not decleare")
-	}
-}
+var regexObject = regexp.MustCompile(`^[A-Z]+:[^ ]+`)
 
 var Analyzer = &analysis.Analyzer{
 	Name: "annotationspace",
@@ -63,27 +36,10 @@ func run(pass *analysis.Pass) (interface{}, error) {
 				for _, c := range n.Comments {
 					if c.List != nil {
 						// check whitespace
-						switch {
-						case strings.Contains(c.Text(), TODO.String()+colon) && !strings.Contains(c.Text(), TODO.String()+colon+whitespace):
-							pass.Reportf(c.Pos(), "require whitespace after TODO:")
-						case strings.Contains(c.Text(), FIXME.String()+colon) && !strings.Contains(c.Text(), FIXME.String()+colon+whitespace):
-							pass.Reportf(c.Pos(), "require whitespace after FIXME:")
-						case strings.Contains(c.Text(), NOTE.String()+colon) && !strings.Contains(c.Text(), NOTE.String()+colon+whitespace):
-							pass.Reportf(c.Pos(), "require whitespace after NOTE:")
-						case strings.Contains(c.Text(), REFUCTOR.String()+colon) && !strings.Contains(c.Text(), REFUCTOR.String()+colon+whitespace):
-							pass.Reportf(c.Pos(), "require whitespace after REFUCTOR:")
+						if regexObject.MatchString(c.Text()) {
+							pass.Reportf(c.Pos(), "require whitespace after annotation comment")
 						}
-						// check colon
-						switch {
-						case strings.Contains(c.Text(), TODO.String()) && !strings.Contains(c.Text(), TODO.String()+colon):
-							pass.Reportf(c.Pos(), "require colon after TODO")
-						case strings.Contains(c.Text(), FIXME.String()) && !strings.Contains(c.Text(), FIXME.String()+colon):
-							pass.Reportf(c.Pos(), "require colon after FIXME")
-						case strings.Contains(c.Text(), NOTE.String()) && !strings.Contains(c.Text(), NOTE.String()+colon):
-							pass.Reportf(c.Pos(), "require colon after NOTE")
-						case strings.Contains(c.Text(), REFUCTOR.String()) && !strings.Contains(c.Text(), REFUCTOR.String()+colon):
-							pass.Reportf(c.Pos(), "require colon after REFUCTOR")
-						}
+						// TODO: check colon
 					}
 				}
 			}
